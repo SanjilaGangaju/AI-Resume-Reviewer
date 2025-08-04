9500a78d50a11edfe6e16b672f75d87e6e5050cc
Sanjila Gangaju <canzilagangaju@gmail.com>
2025-08-04 09:18:09 +0545

    complete navbar

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -3,11 +3,16 @@ import { Link } from 'react-router-dom'
 const Navbar = () => {
   return (
     <>
-    <div>
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="/upload">Upload</Link>
-        <Link to="/login">Login</Link>
+    <div className=' px-15 py-2'>
+      <nav className='flex items-center justify-between'>
+        <div className='text-3xl text-center flex items-center justify-center font-bold'>Resvu AI</div>
+        <div className='flex text-xl font-semibold items-center justify-between gap-6'>
+        <Link className="relative after:absolute after:left-0 after:bottom-0 after:transition-all after:w-0 after:duration-300 after:ease-in-out after:h-[2px] hover:after:w-full after:bg-current"
+ to="/">Home</Link>
+        <Link className='hover-underline' to="/upload">Upload</Link>
+        <Link className='hover-underline' to="/login">Login</Link>
+        </div>
+        
       </nav>
     </div>
     </>
