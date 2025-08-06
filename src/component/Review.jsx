326485ec670bbafc41d443a2fd719ac29ea0bd4c
Sanjila Gangaju <canzilagangaju@gmail.com>
2025-08-06 19:38:34 +0545

    again update on the resume

--- src/component/Review.jsx
+++ src/component/Review.jsx
@@ -38,7 +38,7 @@ const Review = () => {
         const {label, color, bg} = scoreLabel(category.score)
         return (
         <div   key={key}>
-        <div className='flex gap-2 bg-gray-100  px-4 py-2 rounded-full items-center justify-between'><div className='flex gap-4'>
+        <div className='flex gap-2 bg-indigo-50  px-4 py-3 rounded-full items-center justify-between'><div className='flex gap-4'>
           <span className='capitalize text-[0.8rem] text-gray-600'>{key}</span>
           <span className={` flex items-center h-2 tracking-wide justify-center rounded-full p-2 text-[0.45rem] ${bg} text-gray-600 ${color}`}>{label}</span>
           </div><span className={`text-[0.6em] text-gray-500 ${color}`}>{category.score} <span className='text-black'>/ 100</span></span></div>
@@ -47,19 +47,20 @@ const Review = () => {
         
       )})}
       </div>
-      <div>
-        <p>ATS Score: {resumeReview["ATS Compatibility"].score}/100</p>
+      <div className='bg-indigo-50 flex flex-col gap-3 rounded-xl px-5 py-3'>
+        <span className='font-bold text-xl text-gray-500'>ATS Score - {resumeReview["ATS Compatibility"].score}/100</span>
         <p>Great Job!</p>
-        <div className='bg-green-200'>Feedback:<br></br>{resumeReview["ATS Compatibility"].feedback}</div>
+        <p className='text-[0.7rem] text-gray-400'>This score represents how well your resume is likely to perform in Applicant Tracking Systems used by employers</p>
+        <div className='text-blue-300 text-[0.9rem]'>⚡{resumeReview["ATS Compatibility"].feedback}</div>
         {resumeReview["ATS Compatibility"].suggestions?.length>0&&(       
-                <div className='flex flex-col gap-1'>Suggestions: {resumeReview["ATS Compatibility"].suggestions.map(item=>(<span className="bg-blue-100 rounded flex items-center justify-center p-1" key={item}>{item}</span>))}</div>
+                <div className='flex flex-col gap-1'>{resumeReview["ATS Compatibility"].suggestions.map(item=>(<span className="flex text-amber-300 text-[0.9rem]" key={item}>✨ {item}</span>))}</div>
               )}
          {resumeReview["ATS Compatibility"].issues?.length>0 &&(
-              <div className='flex flex-col gap-1'>Issues: {resumeReview["ATS Compatibility"].issues.map(item=>(<span className="bg-red-100 rounded flex items-center justify-center p-1" key={item}>{item}</span>))}</div>
+              <div className='flex flex-col gap-1'>{resumeReview["ATS Compatibility"].issues.map(item=>(<span className="text-[0.9rem] text-red-300 flex" key={item}>⚠️ {item}</span>))}</div>
 
              )}
            {resumeReview["ATS Compatibility"].strengths?.length>0 &&(
-              <div className='flex flex-col gap-1'>Strengths: {resumeReview["ATS Compatibility"].strengths.map(item=>(<span className="bg-red-100 rounded flex items-center justify-center p-1" key={item}>{item}</span>))}</div>
+              <div className='flex flex-col gap-1'>{resumeReview["ATS Compatibility"].strengths.map(item=>(<span className="text-green-600 text-[0.9rem] flex" key={item}>🔥 {item}</span>))}</div>
 
              )}
           
